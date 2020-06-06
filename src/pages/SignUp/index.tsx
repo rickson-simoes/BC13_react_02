74624802e40dfe4a0d5dd5fec24006ae0e24e9a6
Rickson <rickson.simoes@hotmail.com>
2020-06-05 22:35:57 -0300

    :art: Pagina de Cadastro

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Background, Content } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber Logo" />
+
+      <form>
+        <h1>Faça seu Cadastro.</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Nome" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit"> Cadastrar </Button>
+
+        <a href="forgot">Esqueci minha senha</a>
+      </form>
+
+      <a href="create">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
