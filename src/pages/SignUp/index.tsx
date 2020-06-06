82c92edfdc22a91ac71a3dae9e9fae97f5f62b67
Rickson <rickson.simoes@hotmail.com>
2020-06-06 18:35:41 -0300

    :hammer: :wrench: Usabilidade do Input

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { FiArrowLeft, FiMail, FiUser, FiLock, FiPrinter } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -8,35 +9,39 @@ import Button from '../../components/Button';
 
 import { Container, Background, Content } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber Logo" />
+const SignUp: React.FC = () => {
+  function handleSubmit(data: string): any {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber Logo" />
 
-      <form>
-        <h1>Faça seu Cadastro.</h1>
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro.</h1>
 
-        <Input name="name" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-        <Button type="submit"> Cadastrar </Button>
+          <Button type="submit"> Cadastrar </Button>
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
 
-      <a href="create">
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
-    </Content>
-  </Container>
-);
-
+        <a href="create">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 export default SignUp;
